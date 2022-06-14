940370d9837420df1c52deac87d6fcfb2c248af0
Automated Build <ftdr-gitlab-svc@ahs.com>
2022-06-14 16:13:58 -0600

    added validation to main file

--- __main__.py
+++ __main__.py
@@ -1,5 +1,10 @@
 import sys
 from scrape import getGameBoard
+from validate import validate
 
-for pipe in getGameBoard(sys.argv[1] if len(sys.argv) >= 2 else 'https://www.puzzle-pipes.com/'):
-    print(pipe)
+board = getGameBoard(sys.argv[1] if len(sys.argv) >= 2 else 'https://www.puzzle-pipes.com/')
+
+for node in board:
+    print(node)
+
+print("Should be false: ", validate(board))
